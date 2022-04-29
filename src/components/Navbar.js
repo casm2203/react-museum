3f1624ee395f2478720589cbb5e3241a26c8a19a
cristian suarez <casm2203@gmail.com>
2022-04-29 14:49:49 -0500

    redesing_navbar_done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import museum from "../assets/museum2.png";
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-light bg-success">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="https://getbootstrap.com/docs/5.1/components/navbar/">
-                    <img src="https://img.icons8.com/dusk/344/museum.png" alt="" width="60" height="48" className="d-inline-block align-text-top" />
-                <strong  className="text-light" >The Museum</strong>
+        <nav className="navbar navbar-light bg-success bg-gradient shadow-sm  mb-  rounded">
+            <div className="container-fluid d-flex justify-content-center ">
+                <a className="navbar-brand d-flex align-items-center " href="https://getbootstrap.com/docs/5.1/components/navbar/">
+                    <img src={museum} alt="" width="50" height="50" className="d-inline-block align-text-top" />
+                <strong  className="text-light " >The Museum</strong>
                 </a>
             </div>
         </nav>
